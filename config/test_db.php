a17f2c2c4e6a8d1f20573e6c69978697cc0a1f16
topalek <topalek@mail.ru>
2017-06-16 16:59:48 +0300

    params and db merge

--- config/test_db.php
+++ config/test_db.php
@@ -1,5 +1,12 @@
 <?php
-$db = require(__DIR__ . '/db.php');
+$params = \yii\helpers\ArrayHelper::merge(
+	require(__DIR__ . '/params.php'),
+	require(__DIR__ . '/params-local.php')
+);
+$db = \yii\helpers\ArrayHelper::merge(
+	require(__DIR__ . '/db.php'),
+	require(__DIR__ . '/db-local.php')
+);
 // test database! Important not to run tests on production or development databases
 $db['dsn'] = 'mysql:host=localhost;dbname=yii2_basic_tests';
 
